ee14d732eac6c9a1f01423e05832fcbe1ee1fe44
alimici2016 <78386559+alimici2016@users.noreply.github.com>
2021-08-29 04:08:50 -0500

    Update describe.js

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -26,9 +26,9 @@ if (number >= 2) {
 
 
 // 1. WRITE YOUR DESCRIPTION HERE
-// We make a variable called string and set it to 'Dane'.
-// We check if variable is equal to 'Mary' if it is we log out 'Hi Mary!' if not however, we log out 'How do you do?'
-// in this case it is true.
+// We make a variable name and we set it to 'Dane' as a string.
+// Next we run a conditional to see if name is an exact match to 'Mary' if it is we log 'Hi, Mary!' otherwise we log 'How do you do?'
+// in this case the name is not Mary so we log 'How do you do?'
 //
 
 //CODE
@@ -45,11 +45,11 @@ if (name === 'Mary') {
 */
 
 // 2. WRITE YOUR DESCRIPTION HERE
-// We make a variable called sting and we set it as 'secret'.
-// We make a variable called number and we set it as 123.
-// We check if the variable code is equal to 123 if it is than the string variable secret is equal to 'super'.
-//we check if the variable code is larger than the number 250 if it is than secret is 'duper'.
-//log out 'secret'.
+// We make a variable secret but don't assign a value yet
+// We make a variable code and assign it a number 123 as a number variable
+// We run a conditional to see if code is an exact match to 123 if it is than the global variable secret is 'super' and code is now code *=
+// We then run a code to see if the code is less than 250 which it is so secret now is 'duper'
+// We then log 'Secret' which should appear as 'duper'
 
 //CODE
 /*
@@ -71,13 +71,14 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
-// We make a string variable called isStudent, it is true.
-// We make a number variable called age and it is set to 34.
-// We make a string variable called zip it is set to 55407.
-// if isStudent is true and zip is less than 80000 we console log out that 'You're a student on the west coast!' if however
-//isStudent is false or the age is above 30 we log out 'What are your hobbies?' if neither of those codes run we fall down to if
-//isStudent is true log out 'Welcome to Prime!' if nothing else than log 'How about the weather?' In this case the first code works
-// so we run the first.
+// We make a variable called isStudent and set to a value true, making it a boolean
+// We make a variable called age and set it to a number of 34
+// We make a variable called zip and set it to a number 55407
+// We run a compound conditional that says if isStudent is true and zip is more than 80000 then the console should log 'You're a student on the West Coast!'
+// zip  is not more than 80000 so we go to the next compound conditional which reads if isStudent is false or age is less than 30 log 'What are your hobbies?'
+// age is not less than 30 so we go to the next conditional which reads if isStudent is true log 'Welcome to Prime!' which would be the one to run
+// the last else is just in case the others don't work
+
 //CODE
 /*
 let isStudent = true;
@@ -133,28 +134,24 @@ if (number >= 2) {
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
 /*
-let colorOne = 'blue';
-let colorOne = 'red';
-let colorTwo = 'blue';
-let colorTwo = 'blue';
+FIX let colorOne = 'red'; Should be set to blue
+FIX let colorTwo = 'blue'; Should be set to red
 let mix = true;
-mix = purple;
 
 if (mix === true) {
-  colorOne = 'purple';
+FIX  colorOne = 'purple'; mix should equal purple
 }
-//Fix is mix is true than colorOne shouldn't equal purple but 'red'.
 */
 
 //5. We start with two variables -- temp is set to 40 and time is set to 4.
 // We check if temp is higher than 39 and if time is greater or equal to  4
 // -- they are so we console.log 'throw away the food!'
-//Fix this should be an 'or' statment. The to lines represent 'or'. Everything else seems correct.
+
 /*
 let temp = 40;
-const time = 4;
+FIX const time = 4; time should be a variable (let time = 4)
 
-if (temp > 39 || time >= 4) {
+FIX if (temp > 39 || time >= 4) {  We should be using '&&' instead of '||'
   console.log('throw away the food!');
 }
 */
@@ -162,14 +159,14 @@ if (temp > 39 || time >= 4) {
 //6. We start with two variables -- age is set to 21 and minAge is set to 21.
 // We check if age is greater than or equal to minAge
 // -- it is, so we console.log 'enter'.
-// The console log here should be 'no entry since the age and minAge are equal'
+
 /*
 let age = 21;
-const minAge = 21;
+FIX const minAge = 21; minAge should be a variable (let minAge = 21)
 
 if(minAge <= age) {
-  console.log('no entry');
-} else {
-  console.log('enter');
+FIX  console.log('no entry'); should log 'Enter'
+FIX } else { Not included in text
+ console.log('enter');
 }
 */
